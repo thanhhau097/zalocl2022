cb4fad4813348ebd7ffa8b8494eff5a70f2cf270
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2022-11-10 18:12:57 +0700

    add external

--- data_args.py
+++ data_args.py
@@ -12,5 +12,13 @@ class DataArguments:
         default="data/train/labels/",
         metadata={"help": "Label folder"},
     )
+    external_audio_folder: str = field(
+        default="/home/thanh/shared_disk/thanh/chunks/", metadata={"help": "External audio folder"}
+    )
+    external_label_folder: str = field(
+        default="/home/thanh/shared_disk/thanh/chunk_labels/",
+        metadata={"help": "External label folder"},
+    )
+    use_external: bool = field(default=True, metadata={"help": "Use external data"})
     sample_rate: int = field(default=16000, metadata={"help": "Audio sample rate"})
     max_length: int = field(default=480000, metadata={"help": "Audio max length"})
