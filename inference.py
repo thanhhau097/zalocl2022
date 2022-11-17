93dcd051e425ca1c9f4e4237853bbffad3568125
Thanh Hau Nguyen <t.nguyen@sprout.ai>
2022-11-17 04:30:20 +0000

    update inference

--- inference.py
+++ inference.py
@@ -20,7 +20,7 @@ torch.set_grad_enabled(False)
 woptions = whisper.DecodingOptions(language="vi", without_timestamps=True)
 wtokenizer = whisper.get_tokenizer(True, language="vi", task=woptions.task)
 model = WhisperModel("base")
-checkpoint = torch.load("wbase/checkpoint-5733/pytorch_model.bin", "cpu")
+checkpoint = torch.load("./wbase/checkpoint-89300/pytorch_model.bin", "cpu")
 model.load_state_dict(checkpoint)
 model = model.cuda()
 model.eval()
@@ -59,94 +59,22 @@ for audio_name in tqdm(os.listdir(TEST_AUDIO_FOLDER)):
 
     input_ids = torch.from_numpy(np.array(mel)).unsqueeze(0).cuda()
     dec_input_ids = torch.from_numpy(np.array(separated_tokens)).unsqueeze(0).cuda()
-    word_idxs = [word_idxs]
+    word_idxs = torch.from_numpy(np.array(word_idxs)).unsqueeze(0).cuda()
 
-    out = model(input_ids, dec_input_ids)
-    for i, res in enumerate(out):
-        prediction = {}
-        words_dict = defaultdict(list)
-        word_ids_dict = defaultdict(list)
-
-        for token_id, word_id, (s, e) in zip(dec_input_ids[i], word_idxs[i], res):
-            token = wtokenizer.decode(token_id)
-            prediction[str(word_id) + token] = [s.item(), e.item()]
-            # print(token, s.item() * max_ms, e.item() * max_ms)
-            words_dict[word_id].append(token)
-            word_ids_dict[word_id].append(token_id)
-
-        # print(prediction)
-        words = list(words_dict.values())
-        word_ids = list(word_ids_dict.values())
-        output_dict = {}
-        output_words = []
-        output_times = []
-
-        for wid, word_tokens, word_id in zip(words_dict.keys(), words_dict.values(), word_ids):
-            starts = []
-            ends = []
-
-            pred = [float("inf"), 0]
-            for token in word_tokens:
-                # prediction
-                s, e = prediction[str(wid) + token]
-                # print(s, e)
-                if s < pred[0]:
-                    pred[0] = s
-                if e > pred[1]:
-                    pred[1] = e
-
-            # word = "".join(word_tokens)
-            word = wtokenizer.decode(word_id)
-            time = [int(pred[0] * max_ms), int(pred[1] * max_ms)]
-            # print(word, time)
-            output_dict[word] = time
-            output_words.append(word)
-            output_times.append(time)
-
-    # print(output_dict)
-    # print(output_words)
-    # print(output_times)
-    # convert output_dict to submission format
+    out = model(input_ids, dec_input_ids, word_idxs)
+    
+    # word output
+    out = out[0].tolist()
     for segment in template:
-        min_s, max_e = float("inf"), 0
         for chunk in segment["l"]:
-            s, e = output_times.pop(0)
-            word = output_words.pop(0)
-
+            s, e = out.pop(0)
+            word = words.pop(0)
             if word != chunk["d"].lower():
-                chunk_words = chunk["d"].split()
-                try:
-                    chunk_word = chunk_words.pop(0)
-                except:
-                    output_words.insert(0, word)
-                    break
                 print("Error:", chunk["d"])
-                while chunk_words:
-                    try:
-                        word = output_words.pop(0)
-                        last_s, last_e = output_times.pop(0)
-                        chunk_word = chunk_words.pop(0)
-                        e = last_e
-                        if chunk_word != word:
-                            import pdb
+                continue
 
-                            pdb.set_trace()
-                    except:
-                        break
-
-                # import pdb; pdb.set_trace()
-                # raise Exception()
-
-            chunk["s"] = s
-            chunk["e"] = e
-
-            if s < min_s:
-                min_s = s
-            if e > max_e:
-                max_e = e
-
-        segment["s"] = min_s
-        segment["e"] = max_e
+            chunk["s"] = int(s * 30000)
+            chunk["e"] = int(e * 30000)
 
     # print(template)
     with open(os.path.join(SUBMISSION_FOLDER, name + ".json"), "w") as f:
