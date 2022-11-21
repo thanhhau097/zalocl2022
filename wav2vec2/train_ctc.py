f972776cf9688f082c9f8b86ac7b041e5d8a772d
Thanh Hau Nguyen <t.nguyen@sprout.ai>
2022-11-21 12:17:20 +0000

    update

--- wav2vec2/train_ctc.py
+++ wav2vec2/train_ctc.py
@@ -663,12 +663,12 @@ def main():
         def is_audio_in_length_range(length):
             return length > min_input_length and length < max_input_length
 
-        # # filter data that is shorter than min_input_length
-        # vectorized_datasets = vectorized_datasets.filter(
-        #     is_audio_in_length_range,
-        #     num_proc=num_workers,
-        #     input_columns=["input_length"],
-        # )
+        # filter data that is shorter than min_input_length
+        vectorized_datasets = vectorized_datasets.filter(
+            is_audio_in_length_range,
+            num_proc=num_workers,
+            input_columns=["input_length"],
+        )
         print("vectorizing datasets DONE")
 
 
